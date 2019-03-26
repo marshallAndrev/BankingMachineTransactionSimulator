4305435a3a23148039e5b60d5d7cade2a59f0537
Marcin Wojcik <kontakt.marcinwojcik@gmail.com>
2019-03-26 18:07:15 +0100

    Add quit option only if user choose 0

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -5,33 +5,46 @@ public class BankAccount {
     public static void main(String[] args) {
 
         int userChoose;
+        float balance = 0f;
+        float amount;
+        boolean quit = false;
+
         Scanner scanner = new Scanner(System.in);
+        
+        do {
+            System.out.println("Choose your operation type: ");
+            System.out.println("Deposit money - press 1");
+            System.out.println("Withdrawl money - press 2");
+            System.out.println("Saldo check - press 3");
+            System.out.println("Quit - press 0");
 
-        System.out.println("Choose your operation type: ");
-        System.out.println("deposit money - press 1");
-        System.out.println("withdrawl money - press 2");
-        System.out.println("saldo check - press 3");
-        System.out.println("quit - press 0");
+            userChoose = scanner.nextInt();
 
-        userChoose = scanner.nextInt();
-
-        switch (userChoose) {
-            case 1:
-                //deposit money
-                break;
-            case 2:
-                //withdrawl money
-                break;
-            case 3:
-                //saldo check
-                break;
-            case 0:
-                //quit
-                break;
-            default:
-                System.out.println();
-                break;
-        }
+            switch (userChoose) {
+                case 1:
+                    System.out.println("Amount to deposit: ");
+                    amount = scanner.nextFloat();
+                    balance = amount + balance;
+                    System.out.println("Your balance: " + balance + "$");
+                    break;
+                case 2:
+                    System.out.println("Amount to winthdrawl: ");
+                    amount = scanner.nextFloat();
+                    balance = amount - balance;
+                    System.out.println("Your balance: " + balance + "$");
+                    break;
+                case 3:
+                    System.out.println("Your balance: " + balance + "$");
+                    break;
+                case 0:
+                    quit = true;
+                    break;
+                default:
+                    System.out.println();
+                    break;
+            }
+        } while (!quit);
+        System.out.println("Bye bye");
 
     }
 }
